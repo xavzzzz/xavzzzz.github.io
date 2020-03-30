7f8b9575a55115b41f5a02bbaaeb744efa15d639
xavzzzz <36257240+xavzzzz@users.noreply.github.com>
2020-03-30 15:08:42 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Page Title</title>
+<meta name=”description” content=”Description”>
+<meta name=”keywords” content=”Keyword1, Keyword2″>
+</head>
+<frameset rows=”100%,0″ border=”0″>
+<frame src=”https://xd.adobe.com/view/b2cb3677-6bfe-4f90-71d0-cf8ddb384f72-c4fa/?fullscreen” frameborder=”0″>
+<frame frameborder=”0″>
+</frameset>
+</html>
